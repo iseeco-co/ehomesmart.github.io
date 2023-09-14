b2c246f8591edcf2b8268b089d635185aed23a25
hassan - mac <hne1991@gmail.com>
2023-09-14 19:03:22 +0330

    first commit

---
+++ precache-manifest.22b7f8c77434871f223f74d07ea178ee.js
@@ -0,0 +1,210 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee80111cc3ffcf9d788ea0e8f3d74a8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59da2d081e008a94aed8",
+    "url": "/static/css/main.c7126ec6.chunk.css"
+  },
+  {
+    "revision": "7dcebe92d0e71ccebcbb",
+    "url": "/static/js/2.a09c08e0.chunk.js"
+  },
+  {
+    "revision": "59da2d081e008a94aed8",
+    "url": "/static/js/main.696c7d3b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e5e9172fc240993bd699f593370b2a56",
+    "url": "/static/media/Iphone-spinner-2.e5e9172f.gif"
+  },
+  {
+    "revision": "31ad3099071d0981f5bfcc2d480ee731",
+    "url": "/static/media/Shabnam.31ad3099.ttf"
+  },
+  {
+    "revision": "46e7d4169d218ddfaa17c5e49805e9a2",
+    "url": "/static/media/Shabnam.46e7d416.woff"
+  },
+  {
+    "revision": "7b23666bae662ec6d8d7c66c4e2b2964",
+    "url": "/static/media/Shabnam.7b23666b.eot"
+  },
+  {
+    "revision": "dc59fa37f0317ef9ad0019d018a74483",
+    "url": "/static/media/Shabnam.dc59fa37.woff2"
+  },
+  {
+    "revision": "3ebce5ddce58c181ea40c4d50548389c",
+    "url": "/static/media/air-conditioner-active.3ebce5dd.png"
+  },
+  {
+    "revision": "7a4283b3d1e1233b60765a0edb5398e5",
+    "url": "/static/media/bed-active.7a4283b3.png"
+  },
+  {
+    "revision": "df29d628e6b11e87c42fc196fdffb132",
+    "url": "/static/media/bulb-active.df29d628.png"
+  },
+  {
+    "revision": "63510e2414c424d210ff41348fdae55e",
+    "url": "/static/media/bulb-dark.63510e24.png"
+  },
+  {
+    "revision": "d92780f1f405ebcf443e844456b705fd",
+    "url": "/static/media/chandelier-active.d92780f1.png"
+  },
+  {
+    "revision": "f093ed311b8bb5519b407fea553642bb",
+    "url": "/static/media/chandelier-dark.f093ed31.png"
+  },
+  {
+    "revision": "4cf3a119f719ea927f11b6503214625a",
+    "url": "/static/media/cinema-active.4cf3a119.png"
+  },
+  {
+    "revision": "35ff1b485992947eca9f745f948ea97a",
+    "url": "/static/media/cinema-dark.35ff1b48.png"
+  },
+  {
+    "revision": "29ec368e41cc5e156bc46fd13f0d5336",
+    "url": "/static/media/dancing-active.29ec368e.png"
+  },
+  {
+    "revision": "c542dca81b30fc986349a7d1372f1110",
+    "url": "/static/media/dancing-dark.c542dca8.png"
+  },
+  {
+    "revision": "4e7ca30f42c03be90c03c0a5fe9f7422",
+    "url": "/static/media/ehomesmart.4e7ca30f.png"
+  },
+  {
+    "revision": "1f4260362a2534070723eb49d21a39a1",
+    "url": "/static/media/elevator-active.1f426036.png"
+  },
+  {
+    "revision": "f1998e5e0fcd4eba55869277b961b2da",
+    "url": "/static/media/exit-active.f1998e5e.png"
+  },
+  {
+    "revision": "5bf128f18738e3d3b08ba0bde89f15d9",
+    "url": "/static/media/exit-dark.5bf128f1.png"
+  },
+  {
+    "revision": "187db4ea4fef30e1b9d3328dc8bd1da2",
+    "url": "/static/media/fan-active.187db4ea.png"
+  },
+  {
+    "revision": "2c222df3e4fdcc6ed6cd314807ade50f",
+    "url": "/static/media/fan-dark.2c222df3.png"
+  },
+  {
+    "revision": "0ceaff90f0375e0ee53ab7bfdb98b191",
+    "url": "/static/media/flight-active.0ceaff90.png"
+  },
+  {
+    "revision": "57b92f125e7a8b9db3325a65b7c92b7c",
+    "url": "/static/media/flight-dark.57b92f12.png"
+  },
+  {
+    "revision": "8c504c87fedc45a2835ca82a0cbf62cd",
+    "url": "/static/media/food-active.8c504c87.png"
+  },
+  {
+    "revision": "d7072044b255cc1719a78292e912e92a",
+    "url": "/static/media/food-dark.d7072044.png"
+  },
+  {
+    "revision": "cb77e51d90bc46f21303d91df0ac6bfb",
+    "url": "/static/media/fountain-active.cb77e51d.png"
+  },
+  {
+    "revision": "6f88a59684a0e056820ee97f4d414bab",
+    "url": "/static/media/fountain-dark.6f88a596.png"
+  },
+  {
+    "revision": "e381a31537bae74fc447ea571937e897",
+    "url": "/static/media/iseeco.e381a315.png"
+  },
+  {
+    "revision": "17bddbae7a65cff7cf39a9f24c485995",
+    "url": "/static/media/lock-active.17bddbae.png"
+  },
+  {
+    "revision": "f11e682f1d32db65d9a63b3585a88550",
+    "url": "/static/media/lock-dark.f11e682f.png"
+  },
+  {
+    "revision": "c851c5f87227e79d070b638282d029df",
+    "url": "/static/media/music-active.c851c5f8.png"
+  },
+  {
+    "revision": "38d2be95408f7f593af6cfa74436ea51",
+    "url": "/static/media/music-dark.38d2be95.png"
+  },
+  {
+    "revision": "b172d563a2acdc48899089373d35dfa9",
+    "url": "/static/media/night-active.b172d563.png"
+  },
+  {
+    "revision": "0fc0957037e4c15ce3ee85aa50dd37c1",
+    "url": "/static/media/night-dark.0fc09570.png"
+  },
+  {
+    "revision": "8c0f74269d66d637c65562afe519c1d5",
+    "url": "/static/media/socket-active.8c0f7426.png"
+  },
+  {
+    "revision": "0d6c292544132469890a0090b168aa72",
+    "url": "/static/media/socket-dark.0d6c2925.png"
+  },
+  {
+    "revision": "c48bb482142fb97698955bc21c1bf82c",
+    "url": "/static/media/star-active.c48bb482.png"
+  },
+  {
+    "revision": "3be68368f725b39fed6a0eb2c71f2d13",
+    "url": "/static/media/star-dark.3be68368.png"
+  },
+  {
+    "revision": "2553893c7a18a2f73a60e93457bba2c2",
+    "url": "/static/media/the-way-active.2553893c.png"
+  },
+  {
+    "revision": "2f344538522c7abc56f7588b40f39cb5",
+    "url": "/static/media/thermostat-active.2f344538.png"
+  },
+  {
+    "revision": "353c13269cd3a1cb376dfa0a364ee3d8",
+    "url": "/static/media/thermostat-dark.353c1326.png"
+  },
+  {
+    "revision": "6e199637aaf89267bc387e94cdbd62a2",
+    "url": "/static/media/toilet-active.6e199637.png"
+  },
+  {
+    "revision": "a861383c0c4dbbcfe3b1c927017d19d3",
+    "url": "/static/media/washing-active.a861383c.png"
+  },
+  {
+    "revision": "8e9cb923fc39b861c3ac55c7a0d3d6ba",
+    "url": "/static/media/weather-active.8e9cb923.png"
+  },
+  {
+    "revision": "1ada6b35522bdcc1a4b25c8fc3092dd7",
+    "url": "/static/media/weather-dark.1ada6b35.png"
+  },
+  {
+    "revision": "bfe43132c208d5dba8f816ca8ed0f225",
+    "url": "/static/media/window-active.bfe43132.png"
+  },
+  {
+    "revision": "6fdd72906f4fbbd4ba50d55354c9e37a",
+    "url": "/static/media/window-dark.6fdd7290.png"
+  }
+]);
